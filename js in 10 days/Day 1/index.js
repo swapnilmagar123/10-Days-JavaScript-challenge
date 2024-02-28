972d76fcfc43ef7fa91e2a68284e16564f71082c
Swapnil Magar <140142924+swapnilmagar123@users.noreply.github.com>
2024-02-28 09:25:21 +0530

    Add files via upload

---
+++ js in 10 days/Day 1/index.js
@@ -0,0 +1,35 @@
+let af = 224;
+console.log(typeof(af)); // number
+// let see the oparatets in the js
+let bf = 10 + 5
+console.log(bf);
+ // output= 15
+a = 12;
+b =12;
+console.log(a+b)
+console.log(a-b)
+console.log(a/b)
+console.log(a*b)
+console.log(a**b)
+console.log(a%b)
+// not a number is the also important concept 
+// operaters precidance
+// Parentheses: ( )
+// Exponentiation: **
+// Multiplication and Division: *, /
+// Addition and Subtraction: +, -
+// Comparison Operators: ==, !=, ===, !==, <, >, <=, >=
+// write the js program for check even or odd
+let number = 12;
+if(number%2==0){
+    console.log(number+" is even");
+}
+else
+    { console.log("the  number is  "+number+ "is odd")}
+//  write the Program for area of the circle
+let rad=3;
+let pi =Math.PI;
+let area = pi*rad**2;
+console.log(area);
+
+
